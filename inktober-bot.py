4c136e3b3a243718bd551d79647abf259b95a6cd
이이레 <krbingpop@gmail.com>
2021-09-27 21:12:59 -0400

    fixed logic in adding words

--- inktober-bot.py
+++ inktober-bot.py
@@ -1,81 +1,101 @@
-from os.path import dirname, basename, isfile
-import sys
-import glob
-import os
-import builtins
-import importlib
-import inspect
-from enum import Enum
+import discord
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext, SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
+from builtins import bot, guild_ids
+import random
+import math
 import json
-from discord import Client, Intents, Embed
-from discord_slash import SlashCommand, SlashContext
-from discord.ext.commands import Bot
+import os
+from titlecase import titlecase
 
-# attempt to import discord.py
-try:
-  import discord
-  from discord.ext import commands
-except ImportError:
-  print("discord.py was not found.")
-  sys.exit(1)
-
-# getting directory information
-dirname = os.path.dirname(__file__)
-settings_file_path = os.path.join(dirname, '_config/settings.json')
-modules_path = os.path.join(dirname, 'modules/')
-
-# opening initial settings
-def get_config():
-  with open(settings_file_path) as json_data:
-    return json.load(json_data)
-
-def update_config(config):
-  with open(settings_file_path) as json_out:
-    json.dump(config, json_out, indent = 2)
-
-def get_prefix(bot, ctx):
-  try:
-    guild = str(ctx.guild.id)
-  except:
-    return ""
-  prefixes = get_config()["PREFIXES"]
-  return prefixes.get(guild, "yv ")
-
-# load intents
-intents = Intents.default()
-
-# load the token, prefixes, and intents
-TOKEN = get_config()["TOKEN"]
-#bot = commands.Bot(command_prefix = get_prefix, intents = intents)
-bot = Bot(command_prefix = get_prefix, self_bot = True, help_command = None, intents = intents)
-slash = SlashCommand(bot, sync_commands = True)
-guild_ids = [482725089217871893]
-builtins.bot = bot
-builtins.guild_ids = guild_ids
-#bot.remove_command('help')
-
-# imports
-successful_imports = 0
-total_imports = 0
-print("\n")
-for file in os.listdir(modules_path):
-  filename = os.fsdecode(file)
-  if filename != "__init__.py" and filename.endswith(".py"):
-    total_imports += 1
-    try:
-      bot.load_extension("modules." + filename[:-3])
-      print("{} was loaded successfully!".format(filename), flush = True)
-      successful_imports += 1
-    except Exception as e:
-      print("{} had some problems ({}: {})".format(filename, type(e).__name__, e), flush = True)
-
-@bot.event
-async def on_ready():
-  # runs when the bot is fully functional
-  print("\n{}/{} modules loaded".format(successful_imports, total_imports), flush = True)
-  print("Logged in as {} <{}>".format(bot.user.name, bot.user.id), flush = True)
-  print("Running {}".format(discord.__version__), flush = True)
-  print("--------------------------------------------------------", flush = True)
+class Inktober(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
 
 
-bot.run(TOKEN)
+# --------------------------------------------------------------------
+# helper commands
+# --------------------------------------------------------------------
+
+  def get_data_path(self):
+    # returns the filepath of the _data folder
+    return os.path.dirname(__file__) + "/_data/"
+
+
+  def get_words(self):
+    # returns the list of stored words as a set
+    filepath = self.get_data_path()
+    with open(filepath + "words.txt") as word_data:
+      return set(word_data.readlines())
+
+  def get_users(self):
+    # returns the list of all registered users as a dictionary
+    filepath = self.get_data_path()
+    with open(filepath + "users.json") as json_data:
+      return json.load(json_data)
+
+
+  def convert_words_into_list(self, word_string):
+    # separates a single string of word(s) into a set
+    # separates them on comma and leading spaces, then titlecases them
+    words = word_string.split(",")
+    for i in range(len(words)):
+      words[i] = titlecase(words[i].lstrip(' '))
+    return set(words)
+
+
+
+# --------------------------------------------------------------------
+# owner commands
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# word commands
+# --------------------------------------------------------------------
+
+  @cog_ext.cog_slash(name = "word", description = "Submits a word into the Inktober word bank for the current year.", 
+    options = [create_option(
+      name = "word",
+      description = "The word to be added. Multiple words can be added by separating each with a comma (\",\").",
+      option_type = 3,
+      required = True)])
+  async def submit_word(self, ctx, word: str = None):
+    # takes in word(s) from the user and adds them to the word database
+    # words that are already in the database are ignored
+
+    words = self.convert_words_into_list(word)
+    word_database = self.get_words()
+    already_exists = set()
+    word_counter = 0
+
+    with open(self.get_data_path() + "words.txt", "a") as word_file:
+      for word in words:
+        if word in word_database:
+          already_exists.add(word)
+          print("Did not add {}".format(word))
+        else:
+          word_file.write("\n" + word)
+          print("Added {}".format(word))
+          word_counter += 1
+
+    if word_counter > 0:
+      embed = discord.Embed(title = "", description = ":white_check_mark: **{:,}** word(s) have been added.".format(word_counter))
+      if len(already_exists) > 0:
+        embed.add_field(name = "**Warning:**", value = ":warning: **{:,}** word(s) have been ignored since they are already in the word list:\n{}".format(len(already_exists), str(already_exists)))
+    else:
+      embed = discord.Embed(title = "", description = ":no_entry: This/These words are already on the word list. No words have been added:\n{}".format(str(already_exists)))
+    await ctx.send(embed = embed)
+
+
+
+
+# --------------------------------------------------------------------
+# registration commands
+# --------------------------------------------------------------------
+
+
+
+def setup(bot):
+  bot.add_cog(Inktober(bot))
